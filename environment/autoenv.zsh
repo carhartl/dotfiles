268c296e8b9f48c680e9f77b68438abb69275d95
Klaus Hartl <klaus.hartl@gmail.com>
2016-10-11 17:33:22 +0200

    Speedup shell load time
    - Removing support for unused path.zsh files in topic directories
    - Faster autoenv setup (avoid `brew --prefix autoenv`!)

--- environment/autoenv.zsh
+++ environment/autoenv.zsh
@@ -1,4 +1,5 @@
-if [[ -e $(brew --prefix autoenv)/activate.sh ]]
-then
-  source $(brew --prefix autoenv)/activate.sh
+# $(brew --prefix autoenv) => "/usr/local/opt/autoenv", but that's too slow!
+local autoenv_dir="/usr/local/opt/autoenv"
+if [[ -e $autoenv_dir/activate.sh ]]; then
+  source $autoenv_dir/activate.sh
 fi
