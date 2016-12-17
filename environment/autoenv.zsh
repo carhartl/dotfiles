18bb047731fb49856ae70fe646f4df5de23325a8
Klaus Hartl <klaus.hartl@gmail.com>
2016-12-17 21:13:18 +0100

    Break up zsh configuration files
    Making use of startup files that zsh reads.

--- environment/autoenv.zsh
+++
@@ -1,5 +0,0 @@
-# $(brew --prefix autoenv) => "/usr/local/opt/autoenv", but that's too slow!
-local autoenv_dir="/usr/local/opt/autoenv"
-if [[ -e $autoenv_dir/activate.sh ]]; then
-  source $autoenv_dir/activate.sh
-fi
