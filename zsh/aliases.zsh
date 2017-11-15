cb69ec18deb3d007a01a7511e0bd8e7215360aff
Klaus Hartl <klaus.hartl@gmail.com>
2017-11-15 12:54:22 +0100

    Add `x` alias
    Quickly make the last added file executable.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,6 +22,7 @@ alias backbooks="rsync -av --delete ~/Library/Containers/com.apple.BKAgentServic
 alias backnavi="rsync -av --delete ~/Library/Fresh/Backup /Volumes/Backup/maps\ +\ more"
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/iOS\ Simulator.app"
 alias tagged="tag -f \* ."
+alias x="chmod +x $(ls -tr | tail -1)"
 
 # When you forget to use 'sudo', just do 'redo' to rerun the last command using sudo.
 alias redo='sudo $(fc -ln -1)'
