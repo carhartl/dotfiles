7fd2b1a21a397d4944a7bd71d31f645734e97683
Klaus Hartl <klaus.hartl@gmail.com>
2019-11-21 18:02:58 +0100

    Revisit aliases for docker

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,8 +1,8 @@
 alias d="docker \$*"
 alias dc="docker-compose \$*"
-# stop containers
-alias ds="docker stop \$(docker ps -q -f status=exited)"
-# remove stopped containers
-alias dgcc="docker rm -v \$(docker ps -q -f status=exited)"
-# remove dangling images
-alias dgci="docker rmi \$(docker images -q -f dangling=true)"
+# Stop containers
+alias ds="docker stop \$(docker ps -a -q)"
+# Remove stopped containers
+alias dgcc="docker rm -v \$(docker ps -a -q -f status=exited)"
+# Remove dangling images
+alias dgci="docker image prune"
