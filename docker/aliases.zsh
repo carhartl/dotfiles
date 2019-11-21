d3a83793d3fcc4eed778c7cd6f7c3971607f0faf
Klaus Hartl <klaus.hartl@gmail.com>
2019-11-21 18:03:13 +0100

    Add docker alias for removing dangling volumes

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -6,3 +6,5 @@ alias ds="docker stop \$(docker ps -a -q)"
 alias dgcc="docker rm -v \$(docker ps -a -q -f status=exited)"
 # Remove dangling images
 alias dgci="docker image prune"
+# Remove dangling volumes
+alias dgcv="docker volume rm \$(docker volume ls -q -f dangling=true)"
