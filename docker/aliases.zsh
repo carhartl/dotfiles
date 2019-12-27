be3f7da364dda36663bd463b3c4a064dfb8ce89a
Klaus Hartl <klaus.hartl@gmail.com>
2019-12-27 12:23:13 +0100

    Remove no longer needed docker aliases
    We can use `docker system prune`, `docker image prune` etc.

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,9 +1,2 @@
 alias dc="docker-compose"
-# Stop containers
 alias ds="docker stop \$(docker ps -a -q)"
-# Remove stopped containers
-alias dgcc="docker rm -v \$(docker ps -a -q -f status=exited)"
-# Remove dangling images
-alias dgci="docker image prune"
-# Remove dangling volumes
-alias dgcv="docker volume rm \$(docker volume ls -q -f dangling=true)"
