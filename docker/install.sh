2db4af9541b2913c56f0537e02b50edb31db9540
Klaus Hartl <klaus.hartl@gmail.com>
2020-02-19 18:15:02 +0100

    Fix docker zsh completion setup

--- docker/install.sh
+++ docker/install.sh
@@ -3,5 +3,4 @@
 # Activate completion for Docker commands
 etc=/Applications/Docker.app/Contents/Resources/etc
 ln -s $etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
-ln -s $etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
 ln -s $etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
