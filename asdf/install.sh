fbfa631c95b0b1868233e6cefc7832458ca8cd5f
Klaus Hartl <klaus.hartl@gmail.com>
2021-05-02 13:45:31 +0200

    Automate asdf installation further
    Let's setup some default languages.

--- asdf/install.sh
+++ asdf/install.sh
@@ -5,3 +5,10 @@ asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf plugin-add nodejs
 asdf plugin-add python
 asdf plugin-add ruby
+
+# Setup some initial versions
+asdf global nodejs system
+asdf install python 3.9.4
+asdf global python 3.9.4
+asdf install ruby 3.0.1
+asdf global ruby 3.0.1
