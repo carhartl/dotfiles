e9f93d67ece1f0ca9511c549a4317cd09bb01cc2
Klaus Hartl <klaus.hartl@gmail.com>
2022-04-08 18:00:20 +0200

    Add prettier setup to global npm packages
    - IntelliJ IDEA plugin won't pick up homebrew install (needs package)
    - As package I get to have plugins available automatically as well

--- install/node.sh
+++ install/node.sh
@@ -2,4 +2,7 @@
 #
 # Install desired npm packages
 npm install git-unsaved --global --quiet
+npm install prettier-plugin-properties --global --quiet
+npm install prettier-plugin-sh --global --quiet
+npm install prettier --global --quiet
 npm install trash-cli --global --quiet
