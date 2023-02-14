e44ed353115a97e23d06908d07e73329a178bf38
Klaus Hartl <klaus.hartl@gmail.com>
2023-02-14 19:37:22 +0100

    Fix setting up zsh completions for docker
    The `~/.zsh` directory doesn't yet exists..

--- install/docker.sh
+++ install/docker.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
 # Activate completion for Docker commands
+# https://docs.docker.com/desktop/faqs/macfaqs/#how-do-i-install-shell-completion
 etc=/Applications/Docker.app/Contents/Resources/etc
-target=~/.zsh/completion
-mkdir -p $target
-ln -sf $etc/docker.zsh-completion $target/_docker
-ln -sf $etc/docker-compose.zsh-completion $target/_docker-compose
+mkdir -p $HOME/.zsh
+mkdir -p $HOME/.zsh/completion
+ln -sf $etc/docker.zsh-completion $HOME/.zsh/completion/_docker
+ln -sf $etc/docker-compose.zsh-completion $HOME/.zsh/completion/_docker-compose
