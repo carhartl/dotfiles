3b1fc5d7838197dfaa1f41f450ff4416b3b0f5e2
Klaus Hartl <klaus.hartl@gmail.com>
2023-02-14 19:41:29 +0100

    Remove Slate
    - Cask formula doesn't exist anymore
    - Not wanting to install Rosetta on new machine

--- .slate.js
+++
@@ -1,5 +0,0 @@
-slate.on("windowOpened", function (event, win) {
-    if (win.app().name() === "Kaleidoscope") {
-        win.move({ "x": "screenOriginX+((screenSizeX-windowSizeX)/2)", "y": "screenOriginY+((screenSizeY-windowSizeY)/2)" });
-    }
-});
