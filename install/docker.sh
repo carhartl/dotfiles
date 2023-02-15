aa626a2fcfbe88d64026f0d66c6716e30a83fbc9
Klaus Hartl <klaus.hartl@gmail.com>
2023-02-15 09:12:46 +0100

    Simplify docker shell completion setup

--- install/docker.sh
+++ install/docker.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 #
-# Activate completion for Docker commands
+# Install shell completion for Docker commands
 # https://docs.docker.com/desktop/faqs/macfaqs/#how-do-i-install-shell-completion
 etc=/Applications/Docker.app/Contents/Resources/etc
-mkdir -p $HOME/.zsh
-mkdir -p $HOME/.zsh/completion
-ln -sf $etc/docker.zsh-completion $HOME/.zsh/completion/_docker
-ln -sf $etc/docker-compose.zsh-completion $HOME/.zsh/completion/_docker-compose
+ln -s $etc/docker.zsh-completion /opt/homebrew/share/zsh/site-functions/_docker
+ln -s $etc/docker-compose.zsh-completion /opt/homebrew/share/zsh/site-functions/_docker-compose
