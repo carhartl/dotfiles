bf8c57397ff886102dc07b67ac7573237ca20562
Klaus Hartl <klaus.hartl@gmail.com>
2023-12-13 13:11:08 +0100

    Add shfmt install

--- install/go.sh
+++ install/go.sh
@@ -3,3 +3,4 @@
 go install github.com/rakyll/gotest@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/vuln/cmd/govulncheck@latest
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
