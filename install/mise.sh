3512ca42c49838f9af1f524757155402ac49d170
Klaus Hartl <klaus.hartl@gmail.com>
2024-02-09 16:01:29 +0100

    Bring back (updated) mise install script

--- install/mise.sh
+++ install/mise.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #
 # Setup some initial versions
-mise install kubectl@1.25.5
-mise global kubectl@1.25.5
-mise global nodejs@system
+mise use -g kubectl@system
+mise use -g nodejs@lts
 mise install python@3.9.4
-mise global python@3.9.4
-export RUBY_CONFIGURE_OPTS=--with-libyaml-dir=/opt/homebrew/opt/libyaml
+mise use -g python@3.9.4
 mise install ruby@3.2.1
-mise global ruby@3.2.1
+mise use -g ruby@3.2.1
