ce0cfde0e18ae35cd251ac3781d6ddfd37a8553e
Klaus Hartl <klaus.hartl@gmail.com>
2024-10-11 20:23:22 +0200

    Set up default Go packages similar to Ruby
    Once I switch to use mise for managing Go I get the installation for
    free and can remove install/go.sh.

--- install/go.sh
+++ install/go.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-go install github.com/rakyll/gotest@latest
-go install golang.org/x/tools/cmd/goimports@latest
-go install golang.org/x/vuln/cmd/govulncheck@latest
+cat $HOME/.default-go-packages | xargs go install
