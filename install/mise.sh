1b0e77e3e42e91e0e590a8bf15ae25af05411a69
Klaus Hartl <klaus.hartl@gmail.com>
2024-10-11 20:09:20 +0200

    Add mise config to dotfiles

--- install/mise.sh
+++ install/mise.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-#
-# Setup some initial versions
-mise use -g kubectl@system
-mise use -g nodejs@lts
-mise install python
-mise use -g python
-mise install ruby
-mise use -g ruby
-mise install terraform
-mise use -g terraform
+
+mise install
