41d4d3e08e9d4b167b48eb000b6a3ae0b73eee27
Klaus Hartl <klaus.hartl@gmail.com>
2024-10-11 16:25:50 +0200

    Remove globally installed Node packages
    No longer using them, except Prettier installed via Homebrew.

--- install/node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Install desired npm packages
-npm install git-unsaved --location=global --quiet
-npm install prettier-plugin-properties --location=global --quiet
-npm install prettier-plugin-sh --location=global --quiet
-npm install prettier --location=global --quiet
