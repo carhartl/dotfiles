944e5af529f9efa43793f7c4b3f0b9d533a5642c
Klaus Hartl <klaus.hartl@gmail.com>
2024-10-13 07:49:57 +0200

    Remove unused things

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,26 +13,12 @@ alias ws="cd ~/Workspace"
 alias e="\${(z)VISUAL:-\${(z)EDITOR}} \${1:-.}"
 alias o="open"
 alias oo="open ."
-alias heroes="osascript -e 'tell application \"iTunes\" to play the playlist named \"git push\"'"
 alias todos="rg TODO: -uu -g '!*.git' -g '!venv' -g '!node_modules'"
 alias fixmes="rg FIXME: -uu -g '!*.git' -g '!venv' -g '!node_modules'"
 alias pbsort="pbpaste | sort | pbcopy"
 alias backtunes="rsync -av --delete /Volumes/Musik/Music /Volumes/Backup"
-alias backbooks="rsync -av --delete ~/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks /Volumes/Backup"
-alias backnavi="rsync -av --delete ~/Library/Fresh/Backup /Volumes/Backup/maps\ +\ more"
-alias tagged="tag -f \* ."
 alias x="chmod +x $(ls -tr | tail -1)"
 alias jq=jaq
-
-# Suffix aliases
-alias -s css=code
-alias -s html=code
-alias -s js=code
-alias -s md=code
-alias -s mjs=code
-alias -s py=code
-alias -s rb=code
-alias -s tf=code
 
 # Serve files from current directory
 alias serve="python -m http.server"
@@ -46,14 +32,8 @@ alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r 
 # Lists in one column, hidden files
 alias l="ls -1A"
 
-# Lists human readable sizes, hidden files
-alias ll="ls -lAh"
-
 # Update all the things
 alias update="echo '\e[32m==>\e[0m Homebrew...'; brew update; brew upgrade; echo '\e[32m==>\e[0m mise...'; mise up; mise plugins up; echo '\e[32m==>\e[0m Go...'; gup update; echo '\e[32m==>\e[0m Ruby...'; gem update --system; gem update; echo '\e[32m==>\e[0m Python...'; pip install --upgrade pip; echo '\e[32m==>\e[0m Nodejs...'; npm update --location=global npm; npm update --location=global;"
-
-# Clean up LaunchServices to remove duplicates in the “Open With” menu
-alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 
 # View HTTP traffic
 alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
@@ -69,16 +49,5 @@ alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to 
 alias urlencode='python3 -c "import sys, urllib.parse as ul; print(ul.quote_plus(sys.argv[1]))"'
 alias urldecode='python3 -c "import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))"'
 
-# Ring the terminal bell, and put a badge on Terminal.app’s Dock icon
-# (useful when executing time-consuming commands)
-alias badge="tput bel"
-
 # Pretty print all paths
 alias path='echo -e ${PATH//:/\\n}'
-
-# Quicklook
-alias ql='qlmanage -p "$@" >& /dev/null'
-
-# Stuff I never really use but cannot delete either because of http://xkcd.com/530/
-alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume 7'"
