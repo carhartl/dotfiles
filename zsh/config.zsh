c2abbdc125f174a848b130d6fe790c5317ce9d74
Klaus Hartl <klaus.hartl@gmail.com>
2025-01-01 10:39:19 +0100

    Add mitigation/defense-in-depth for CVE-2024-56803
    https://dgl.cx/2024/12/ghostty-terminal-title
    
    (Fixed with Ghostty 1.0.1)

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -91,3 +91,18 @@ bindkey "^[[B" down-line-or-beginning-search
 
 # Do history expansion on space
 bindkey " " magic-space
+
+# https://dgl.cx/2024/12/ghostty-terminal-title
+function skip-osc-sequence() {
+  local key
+  while read -sk key && (( $((#key)) != 0x1B && $((#key)) != 0x07 )); do
+    # empty body
+  done
+  if [[ $((#key)) = 27 ]]; then
+    # ^[\
+    read -sk key
+  fi
+}
+
+zle -N skip-osc-sequence
+bindkey "\e]" skip-osc-sequence
