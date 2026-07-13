c071a81c7cc5007bdddd27d51eb2de3635c04e56
Klaus Hartl <klaus.hartl@gmail.com>
2026-07-13 09:55:19 +0200

    Tweak fzf things again

--- .config/zsh/.zshrc.d/fzf.zsh
+++ .config/zsh/.zshrc.d/fzf.zsh
@@ -1,4 +1,6 @@
-export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git --exclude node_modules'
-export FZF_DEFAULT_OPTS="--style=minimal --preview='bat --color=always --style=plain --line-range=:500 {}'"
+export FZF_DEFAULT_COMMAND="fd --type file --follow --hidden --exclude .git --exclude node_modules"
+export FZF_DEFAULT_OPTS="--style=minimal"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="--layout=reverse --height=70% --preview-window=right:60%"
+export FZF_CTRL_T_OPTS="--layout=reverse --height=70% \
+--preview='bat --color=always --style=plain --line-range=:500 {}' --preview-window=right:60% \
+--bind '?:toggle-preview,ctrl-y:execute-silent(echo {+} | pbcopy),enter:execute(zed {+})'"
