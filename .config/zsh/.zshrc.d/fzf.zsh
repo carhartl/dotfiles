8b7970a3d3ccd4c97e2c4e42a46c641609c44dad
Klaus Hartl <klaus.hartl@gmail.com>
2026-07-20 09:41:38 +0200

    Make fzf behave the same for all invocations

--- .config/zsh/.zshrc.d/fzf.zsh
+++ .config/zsh/.zshrc.d/fzf.zsh
@@ -1,6 +1,6 @@
 export FZF_DEFAULT_COMMAND="fd --type file --follow --hidden --exclude .git --exclude node_modules"
 export FZF_DEFAULT_OPTS="--style=minimal"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="--layout=reverse --height=70% \
+export FZF_CTRL_T_OPTS="--layout=default \
 --preview='bat --color=always --style=plain --line-range=:500 {}' --preview-window=right:60% \
 --bind '?:toggle-preview,ctrl-y:execute-silent(echo {+} | pbcopy),enter:execute(zed {+})'"
